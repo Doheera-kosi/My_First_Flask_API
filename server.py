63b70575bb127b225574cc5ed6e7b55c71d5f6a2
Doheera-kosi <kupourevans99@gmail.com>
2023-04-20 18:49:03 +0000

    Add route and funtion in server.py

--- server.py
+++ server.py
@@ -1,1 +1,10 @@
 from flask import Flask
+
+app = Flask("My Hello World Application")
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__=="__main__":
+    app.run(debug=True)
